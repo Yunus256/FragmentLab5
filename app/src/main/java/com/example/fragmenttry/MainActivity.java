da67670f2b4762b26a7942532e7e754e6ced5ee0
Hak11 <haki_@hotmail.com>
2023-09-15 18:49:05 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/fragmenttry/MainActivity.java
+++ app/src/main/java/com/example/fragmenttry/MainActivity.java
@@ -11,14 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Make changes to Main Activity
-
-        // 3rd changes to the file
-
-        // Changes made by a second team member
-
-        // making changes on the humidity-sensor branch
-
-        // Adding more logic into the 2nd branch
     }
 }
