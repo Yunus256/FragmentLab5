99d8289b677fcbb35b2a70141a4321445c9a0c3b
haki11 <haki_@hotmail.com>
2024-02-01 22:07:33 -0500

    remove comments from the commit

--- app/src/main/java/com/example/fragmenttry/MainActivity.java
+++ app/src/main/java/com/example/fragmenttry/MainActivity.java
@@ -10,14 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Make changes to MainActivity
-
-        // second change by 1st team member
-
-        // Changes made by another team member
-
-        // changes to branch-feature
-
     }
 }
